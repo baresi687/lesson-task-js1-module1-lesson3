b51e4e6f833ee2895e7ea104d36c59389818e37f
hreinn <hreinngy@gmail.com>
2022-01-27 11:40:05 +0100

    Finished tasks

--- js/product.js
+++ js/product.js
@@ -1,5 +1,24 @@
 const product = {
-    name: "Fancy Product",
-    price: 99,
-    onSale: true,
+  name: "Fancy Product",
+  price: 99,
+  onSale: true,
 };
+
+const container = document.querySelector(".container");
+
+let prodName = product.name;
+let prodPrice = product.price;
+let prodSale = product.onSale;
+let underLine = prodSale ? "underline" : "none";
+
+container.innerHTML = `
+<div>
+<h4 style="text-decoration: ${underLine}">${prodName}</h4>
+<p>${prodPrice}</p>
+<p>${prodSale}</p>
+</div>
+`;
+
+const divContent = document.querySelector(".container div");
+divContent.classList.add("product");
+console.log(divContent);
--- js/products.js
+++ js/products.js
@@ -22,24 +22,21 @@ for (let i = 0; i < products.length; i++) {
   let prodName = products[i].name;
   let prodPrice = products[i].price ? products[i].price : "Price not available";
   let prodSouldOut = products[i].soldOut;
-  console.log(products[i].soldOut);
+  let color = "green";
+  color = prodSouldOut ? "red" : color;
+  prodSouldOut = prodSouldOut ? "<b>Not in stock</b>" : "<b>in Stock</b>";
 
   html += `
     <div>
-    <h5>${prodName}</h5>
-    <p>${prodPrice}</p>
+    <h5 style= "color: ${color}" >${prodName}</h5>
+    <p>Price: ${prodPrice}</p>
     <p>${prodSouldOut}</p>
     </div>
     `;
-
-  if (prodSouldOut) {
-    prodName.style.color = "red";
-  } else {
-    prodName.style.color = "green";
-  }
 }
 
 divContainer.innerHTML += html;
+
 const divProducts = document.querySelectorAll(".container div");
 for (let i = 0; i < divProducts.length; i++) {
   divProducts[i].classList.add("product");
